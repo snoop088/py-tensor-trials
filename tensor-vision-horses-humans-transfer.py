6bb61b2748010092da5032398ecff00f76543361
Nick Dimitrov <snoop088@gmail.com>
2023-08-09 12:08:46 +0300

    various tests for the rtx4090

--- tensor-vision-horses-humans-transfer.py
+++ tensor-vision-horses-humans-transfer.py
@@ -1,5 +1,5 @@
-
 import numpy as np
+import tensorrt
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras import layers
 from tensorflow.keras import Model
@@ -12,10 +12,9 @@ from tensorflow.keras.preprocessing import image
 # weights_file = "../data/weights/inception_v3.h5"
 # urllib.request.urlretrieve(weights_url, weights_file)
 
-pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
-                                include_top=False,
-                                weights='imagenet')
-
+pre_trained_model = InceptionV3(
+    input_shape=(150, 150, 3), include_top=False, weights="imagenet"
+)
 
 
 # pre_trained_model.load_weights(weights_file)
@@ -23,42 +22,56 @@ pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
 for layer in pre_trained_model.layers:
     layer.trainable = False
 
-pre_trained_model.summary()
+# pre_trained_model.summary()
 
-last_layer = pre_trained_model.get_layer('mixed5')
-print('last layer output shape: ', last_layer.output_shape)
+last_layer = pre_trained_model.get_layer("mixed7")
+print("last layer output shape: ", last_layer.output_shape)
 last_output = last_layer.output
 
 # Flatten the output layer to 1 dimension
 x = layers.Flatten()(last_output)
 # Add a fully connected layer with 1,024 hidden units and ReLU activation
-x = layers.Dense(1024, activation='relu')(x)
+x = layers.Dense(1024, activation="relu")(x)
 # Add a dropout rate of 0.2
 x = layers.Dropout(0.2)(x)
 # Add a final sigmoid layer for classification
-x = layers.Dense(1, activation='sigmoid')(x)
+x = layers.Dense(1, activation="sigmoid")(x)
 
 model = Model(pre_trained_model.input, x)
-model.compile(optimizer=RMSprop(learning_rate=0.0001), loss="binary_crossentropy", metrics=["acc"])
+model.compile(
+    optimizer=RMSprop(learning_rate=0.0001), loss="binary_crossentropy", metrics=["acc"]
+)
 
-train_datagen = ImageDataGenerator(rescale=1./255.)
-validation_datagen = ImageDataGenerator(rescale=1./255.)
+train_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
+validation_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
 
-train_generator = train_datagen.flow_from_directory('../data/horse-or-human/train', target_size=(150,150), class_mode='binary', batch_size=20)
-validation_generator = validation_datagen.flow_from_directory('../data/horse-or-human/validation', target_size=(150,150), class_mode='binary')
+train_generator = train_datagen.flow_from_directory(
+    "../data/horse-or-human/train",
+    target_size=(150, 150),
+    class_mode="binary",
+    batch_size=20,
+)
+validation_generator = validation_datagen.flow_from_directory(
+    "../data/horse-or-human/validation", target_size=(150, 150), class_mode="binary"
+)
 
 history = model.fit(
-            train_generator,
-            validation_data=validation_generator,
-            epochs=8,
-            verbose=1)
+    train_generator, validation_data=validation_generator, epochs=10, verbose=1
+)
 
 
-
-test_path = '../data/horse-or-human/test/'
-classify = ['pex-1.jpg', 'pex-2.jpg', 'pex-3.jpg', 'pex-4.jpg', 'pex-5.jpg', 'pex-6.jpg', 'pex-7.jpg']
+test_path = "../data/horse-or-human/test/"
+classify = [
+    "pex-1.jpg",
+    "pex-2.jpg",
+    "pex-3.jpg",
+    "pex-4.jpg",
+    "pex-5.jpg",
+    "pex-6.jpg",
+    "pex-7.jpg",
+]
 for file in classify:
-    img = image.load_img(test_path + file, target_size=(150,150))
+    img = image.load_img(test_path + file, target_size=(150, 150))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = x / 255.0
@@ -66,8 +79,6 @@ for file in classify:
     classes = model.predict(image_tensor)
     print(classes[0])
     if classes[0] > 0.5:
-        print(file + ' is a human')
+        print(file + " is a human")
     else:
-        print(file + ' is a horse')
-
-
+        print(file + " is a horse")
--- tensor1.py
+++ tensor1.py
@@ -1,16 +1,31 @@
+import tensorrt
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 
-tf.debugging.set_log_device_placement(True)
+gpus = tf.config.list_physical_devices("GPU")
+if gpus:
+    # Restrict TensorFlow to only use the first GPU
+    try:
+        tf.config.set_visible_devices(gpus[0], "GPU")
+        logical_gpus = tf.config.list_logical_devices("GPU")
 
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    except RuntimeError as e:
+        # Visible devices must be set before GPUs have been initialized
+        print(e)
+
+print(tf.__version__)
+tf.debugging.set_log_device_placement(True)
+device_name = tf.test.gpu_device_name()
+print(device_name)
 model = Sequential([Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sgd', loss='mean_squared_error')
+model.compile(optimizer="sgd", loss="mean_squared_error")
 
 xs = np.array([-1.0, 1.0, 3.0, 5.0, 7.0, 9.0, 10.0], dtype=float)
 ys = np.array([-1.0, 5.0, 11.0, 17.0, 23.0, 29.0, 32.0], dtype=float)
 
-model.fit(xs, ys, epochs=100)
+model.fit(xs, ys, epochs=33)
 
 print(model.predict([20.0]))
